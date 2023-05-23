7725f10bcee26da74962c8029be0797b5d693533
z7016187 <asd880810@gmail.com>
2023-05-23 09:32:01 +0800

    bnn deploy vue

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vuetify from '@vuetify/vite-plugin'
 // https://vitejs.dev/config/
 
 const path = require('path')
 export default defineConfig({
   plugins: [
     vue(),
-
-    vuetify({
-      autoImport: true,
-    }),
   ],
   define: {
     'process.env': {}
