ecf2b626fe93bf85494390ded06ed785661a31be
z7016187 <asd880810@gmail.com>
2023-05-25 23:43:32 +0800

    aaa

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,7 @@ import bodyvue from "./components/body.vue";
 </script>
 
 <template>
-<<<<<<< HEAD
   <headvue />
-=======
-  
-  <headvue></headvue>
->>>>>>> b1d218b7930b2176b99c648526b82e0c2d7c4786
   <bodyvue />
 </template>
 
